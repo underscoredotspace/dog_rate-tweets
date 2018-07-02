b669808821d780b72cdd07adddce354e3051aabd
Glitch (pebble-cone) <none>
2018-07-02 10:42:48 +0100

    code restructure

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,8 @@
 const express = require('express')
 const app = express()
 
-require('dotenv').config()
-
-const credentials = {
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET
-}
-
 const Tweets = require('./model/Tweets')
-const twitter = new Tweets(credentials)
+const twitter = new Tweets
 
 app.get('/', (req, res) => {
   twitter.get((error, tweets) => {
@@ -18,17 +11,7 @@ app.get('/', (req, res) => {
       return
     }
 
-    const tweets_short = tweets.map(tweet => {
-      const {created_at, id_str, full_text} = tweet
-      const tweet_short = {created_at, id_str, full_text}
-
-      if (tweet.extended_entities && tweet.extended_entities.media) {
-        tweet_short.pictures = tweet.extended_entities.media
-      }
-
-      return tweet_short
-    })
-    res.json(tweets_short)
+    res.json(tweets)
   })
 })
 
--- src/server/model/Tweets.js
+++ src/server/model/Tweets.js
@@ -1,7 +1,14 @@
 const Twit = require('twit')
 
+require('dotenv').config()
+
+const credentials = {
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET
+}
+
 class Tweets {
-  constructor(credentials) {
+  constructor() {
     this.credentials = Object.assign({app_only_auth:true}, credentials)
     this.twit = new Twit(this.credentials)
     this.options = {
@@ -15,8 +22,42 @@ class Tweets {
   get(callback, since_id) {
     const options = Object.assign({}, this.options)
     if (since_id) { options = Object.assign(options, {since_id}) }
-    this.twit.get('statuses/user_timeline', options, function(err, data, response) {
-      callback(err, data)
+    this.twit.get('statuses/user_timeline', options, (err, data, response) => {
+      if (err) {
+        callback(err)
+        return
+      }
+
+      this.squash(data).then(tweets_short => {
+        callback(null, data)
+      })
+    })
+  }
+
+  squash(data) {
+    return new Promise((resolve) => {
+      const tweets_short = data.map(tweet => {
+        const {created_at, id_str, full_text, retweet_count, favorite_count} = tweet
+        const tweet_short = {created_at, id_str, full_text, retweet_count, favorite_count}
+
+        if (tweet.extended_entities && tweet.extended_entities.media) {
+          tweet_short.media = tweet.extended_entities.media.map(pic => {
+            const media =  {
+              id: pic.id_str, 
+              url: pic.media_url_https,
+              type: pic.type
+            }
+
+            if (pic.video_info) {
+              media.video_info = pic.video_info
+            }
+
+            return media
+          })
+        }
+      })
+
+      resolve(tweets_short)
     })
   }
 }
